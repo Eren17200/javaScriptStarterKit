03f82d263fbd69fcbd2300ecfa6903576c96f1ce
Alperen <dreamforhaylaz@gmail.com>
2021-10-27 21:12:13 +0300

    26 ekim

---
+++ src/app.js
@@ -0,0 +1,25 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun =9.21
+
+let dolarBugun =9.30
+
+console.log(dolarBugun)
+
+const euroDun = 11
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasign
+
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Krediler"]
+
+console.log("<ul>")
+for(let i= 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri [i]+"</li>")
+    
+}
+console.log("</ul>")
+
+
