59a7e762182cf52f21911a973fc41b1d52ed4a15
Alperen <dreamforhaylaz@gmail.com>
2021-11-16 21:54:51 +0300

    java

--- src/mapFilterReduce.js
+++ src/mapFilterReduce.js
@@ -7,13 +7,19 @@
      {id:1, productName:"Pot", quentity:5, unitPrice: 150}
 
  ]; 
-
+//array in içinde gezmek için
  cart.map(product=>{
     console.log(product.productName + " : " + product.unitPrice * product.quentity)
  })
+
+//toplama yapmak için
+let total=cart.reduce((acc,product)=>acc+ product.unitPrice*product.quentity,0)
  
+console.log(total) 
 
-
+//arama yapmak için
+let quantityOver2=cart.filter(product=>product.quentity>2)
+console.log(quantityOver2)
 /*function addToCart(sepet) {
     cart.push({ id:7, productName:"Ruhsat", quentity:1, unitPrice: 20})
 }
